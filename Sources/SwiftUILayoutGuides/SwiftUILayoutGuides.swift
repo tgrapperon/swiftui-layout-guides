8d1baa3ab4d8ca4d7cf4eea0d50c6eb48044ee14
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2022-04-28 15:29:43 -0300

    Reorganize previews

--- Sources/SwiftUILayoutGuides/SwiftUILayoutGuides.swift
+++ Sources/SwiftUILayoutGuides/SwiftUILayoutGuides.swift
@@ -193,3 +193,68 @@ struct LayoutGuidesModifier: ViewModifier {
     }
   }
 #endif
+
+#if DEBUG
+struct Cell: View {
+  var value: String
+  var body: some View {
+    ZStack {
+      Text(value)
+        .frame(maxWidth: .infinity)
+    }
+    .background(Color.blue.opacity(0.3))
+    .border(Color.blue) // This view fits in readable content width
+    .fitToReadableContentWidth()
+    .border(Color.red) // This view is unconstrained
+  }
+}
+
+struct ListTest: View {
+  var body: some View {
+    List {
+      ForEach(0 ..< 30) {
+        Cell(value: "\($0)")
+      }
+    }
+  }
+}
+
+struct ScrollViewTest: View {
+  var body: some View {
+    ScrollView {
+      VStack(spacing: 0) {
+        ForEach(0 ..< 30) {
+          Cell(value: "\($0)")
+        }
+      }
+    }
+  }
+}
+
+struct SwiftUILayoutGuides_Previews: PreviewProvider {
+  static func sample<Content>(_ title: String, _ content: () -> Content) -> some View
+  where Content: View {
+    VStack(alignment: .leading) {
+      Text(title)
+        .font(Font.system(size: 20, weight: .bold))
+        .padding()
+      content()
+    }
+    .border(Color.primary, width: 2)
+  }
+  
+  static var previews: some View {
+    VStack(spacing: 0) {
+      sample("ScrollView") { ScrollViewTest() }
+      sample("List.plain") { ListTest().listStyle(.plain) }
+      #if os(iOS) || os(tvOS)
+      sample("List.grouped") { ListTest().listStyle(.grouped) }
+      if #available(iOS 14.0, *) {
+      sample("List.insetGrouped") { ListTest().listStyle(.insetGrouped) }
+      }
+      #endif
+    }
+   .previewDevice("iPad Pro (11-inch) (3rd generation)")
+  }
+}
+#endif
